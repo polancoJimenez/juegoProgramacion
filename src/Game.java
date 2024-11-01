49ab4fb4b537b32a9e3bb9cf55a6993267b305ae
polancoJimenez <arenasjimenez@gmail.com>
2024-11-01 00:20:01 -0500

    referencia para hacerlo

--- src/Game.java
+++ src/Game.java
@@ -1,130 +1,120 @@
+//referencia
 import java.util.Random;
 import java.util.Scanner;
 
+// Clase principal del juego
 public class Game {
+    // Atributos del héroe y enemigo
+    private String nombreHeroe;
+    private int saludHeroe;
+    private int ataqueHeroe;
+    private int defensaHeroe;
 
-    // Clase Heroe
-    static class Heroe {
-        private int ataque;
-        private int defensa;
-        private int salud = 100;
-        private boolean defendiendo = false;
+    private String nombreEnemigo;
+    private int saludEnemigo;
+    private int ataqueEnemigo;
+    private int defensaEnemigo;
 
-        public Heroe(int ataque, int defensa) {
-            this.ataque = ataque;
-            this.defensa = defensa;
-        }
+    private Random aleatorio;
+    private Scanner scanner;
 
-        public int getSalud() {
-            return salud;
-        }
+    // Constructor del juego
+    public Game() {
+        this.nombreHeroe = "DOOM";
+        this.saludHeroe = 100;
+        this.ataqueHeroe = 25;
+        this.defensaHeroe = 10;
 
-        public void recibirDanio(int danio) {
-            if (defendiendo) {
-                danio -= defensa / 2; // Reduce el daño a la mitad si se defiende
-                System.out.println("¡El Héroe se defiende! Daño reducido a " + danio);
-                defendiendo = false; // Resetea la defensa después de usarla
+        this.nombreEnemigo = "HANK";
+        this.saludEnemigo = 80;
+        this.ataqueEnemigo = 20;
+        this.defensaEnemigo = 8;
+
+        this.aleatorio = new Random();
+        this.scanner = new Scanner(System.in);
+    }
+
+    // Método para ejecutar el juego
+    public void iniciarJuego() {
+        System.out.println("¡Bienvenido al juego!");
+        System.out.println("¡Comienza la batalla entre " + nombreHeroe + " y " + nombreEnemigo + "!\n");
+
+        while (saludHeroe > 0 && saludEnemigo > 0) {
+            mostrarEstado();
+            int opcion = elegirAccion();
+
+            switch (opcion) {
+                case 1:
+                    atacarEnemigo();
+                    break;
+                case 2:
+                    defenderHeroe();
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intenta nuevamente.");
+                    continue;
             }
-            int danioReal = Math.max(0, danio);
-            salud = Math.max(0, salud - danioReal);
-            System.out.println("Salud del Héroe: " + salud);
+
+            if (saludEnemigo > 0) {
+                atacarHeroe();
+            }
+            System.out.println();
         }
 
-        public void atacar(Villano villano) {
-            System.out.println("¡El Héroe ataca al Villano!");
-            villano.recibirDanio(ataque);
-        }
+        mostrarResultado();
+    }
 
-        public void defender() {
-            defendiendo = true;
-            System.out.println("¡El Héroe se está defendiendo!");
+    // Método para mostrar el estado de ambos personajes
+    private void mostrarEstado() {
+        System.out.println("Salud de " + nombreHeroe + ": " + saludHeroe);
+        System.out.println("Salud de " + nombreEnemigo + ": " + saludEnemigo);
+    }
+
+    // Método para elegir acción
+    private int elegirAccion() {
+        System.out.println("¿Qué quieres hacer?");
+        System.out.println("1. Atacar");
+        System.out.println("2. Defenderse");
+        System.out.print("Elige una opción: ");
+        return scanner.nextInt();
+    }
+
+    // Método para atacar al enemigo
+    private void atacarEnemigo() {
+        System.out.println(nombreHeroe + " ataca a " + nombreEnemigo);
+        int dano = ataqueHeroe - defensaEnemigo;
+        if (dano < 0) dano = 0; // Evita que el daño sea negativo
+        saludEnemigo -= dano;
+        System.out.println(nombreEnemigo + " recibe " + dano + " puntos de daño.");
+    }
+
+    // Método para defenderse
+    private void defenderHeroe() {
+        defensaHeroe += 5;  // Aumenta la defensa temporalmente
+        System.out.println(nombreHeroe + " se defiende, aumentando su defensa en 5 puntos.");
+    }
+
+    // Método para que el enemigo ataque al héroe
+    private void atacarHeroe() {
+        System.out.println(nombreEnemigo + " ataca a " + nombreHeroe);
+        int dano = ataqueEnemigo - defensaHeroe;
+        if (dano < 0) dano = 0; // Evita que el daño sea negativo
+        saludHeroe -= dano;
+        System.out.println(nombreHeroe + " recibe " + dano + " puntos de daño.");
+    }
+
+    // Método para mostrar el resultado final
+    private void mostrarResultado() {
+        if (saludHeroe > 0) {
+            System.out.println("¡" + nombreHeroe + " ha ganado la batalla!");
+        } else {
+            System.out.println("¡" + nombreEnemigo + " ha ganado la batalla!");
         }
     }
 
-    // Clase Villano
-    static class Villano {
-        private int ataque;
-        private int defensa;
-        private int salud = 100;
-        private boolean defendiendo = false;
-
-        public Villano(int ataque, int defensa) {
-            this.ataque = ataque;
-            this.defensa = defensa;
-        }
-
-        public int getSalud() {
-            return salud;
-        }
-
-        public void recibirDanio(int danio) {
-            if (defendiendo) {
-                danio -= defensa / 2; // Reduce el daño a la mitad si se defiende
-                System.out.println("¡El Villano se defiende! Daño reducido a " + danio);
-                defendiendo = false; // Resetea la defensa después de usarla
-            }
-            int danioReal = Math.max(0, danio);
-            salud = Math.max(0, salud - danioReal);
-            System.out.println("Salud del Villano: " + salud);
-        }
-
-        public void atacar(Heroe heroe) {
-            System.out.println("¡El Villano ataca al Héroe!");
-            heroe.recibirDanio(ataque);
-        }
-
-        public void defender() {
-            defendiendo = true;
-            System.out.println("¡El Villano se está defendiendo!");
-        }
-    }
-
+    // Método principal
     public static void main(String[] args) {
-        Heroe heroe = new Heroe(15, 5);
-        Villano villano = new Villano(12, 6);
-        Scanner escaner = new Scanner(System.in);
-        Random aleatorio = new Random();
-
-        // Asignación aleatoria de personaje al usuario
-        boolean usuarioEsHeroe = aleatorio.nextBoolean();
-        String personajeUsuario = usuarioEsHeroe ? "Héroe" : "Villano";
-        System.out.println("¡Felicidades! Has ganado el puesto de " + personajeUsuario + ". ¡Buena suerte!");
-        System.out.println("¡Comienza la batalla!");
-
-        while (heroe.getSalud() > 0 && villano.getSalud() > 0) {
-            System.out.println("Elige una acción:");
-            System.out.println("1. " + personajeUsuario + " ataca");
-            System.out.println("2. " + personajeUsuario + " se defiende");
-
-            int eleccion = escaner.nextInt();
-            if (eleccion == 1) {
-                if (usuarioEsHeroe) {
-                    heroe.atacar(villano);
-                    if (villano.getSalud() > 0) villano.atacar(heroe);
-                } else {
-                    villano.atacar(heroe);
-                    if (heroe.getSalud() > 0) heroe.atacar(villano);
-                }
-            } else if (eleccion == 2) {
-                if (usuarioEsHeroe) {
-                    heroe.defender();
-                    if (villano.getSalud() > 0) villano.atacar(heroe);
-                } else {
-                    villano.defender();
-                    if (heroe.getSalud() > 0) heroe.atacar(villano);
-                }
-            } else {
-                System.out.println("Opción inválida. Por favor, selecciona 1 o 2.");
-            }
-
-            System.out.println("----------------------------");
-
-            if (heroe.getSalud() == 0 || villano.getSalud() == 0) {
-                break;
-            }
-        }
-
-        System.out.println(heroe.getSalud() > 0 ? "¡El Héroe ha ganado!" : "¡El Villano ha ganado!");
-        escaner.close();
+        Game juego = new Game();
+        juego.iniciarJuego();
     }
 }
